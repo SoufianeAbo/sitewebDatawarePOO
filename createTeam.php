234ce30a6af333c41709627ff4bb52c136c24df9
SoufianeAbo <abounasrsoufiane@gmail.com>
2023-12-21 16:12:04 +0100

    finish making create team function for scrummaster

--- createTeam.php
+++ createTeam.php
@@ -1,45 +1,10 @@
 <?php
 session_start();
 include 'connection.php';
+include './includes/scrumMaster.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $formName = $_POST['formName'];
-    $formDescription = $_POST['formDescription'];
-
-    $uploadDir = './img/';
-    $teamImage = $_FILES['teamImage'];
-
-    $uploadPath = $uploadDir . basename($teamImage['name']);
-
-    if ($teamImage['size'] > 0 && $teamImage['size'] <= 10 * 1024 * 1024 && ($teamImage['type'] === 'image/jpeg' || $teamImage['type'] === 'image/png')) {
-        list($width, $height) = getimagesize($teamImage['tmp_name']);
-        if ($width >= 1152 && $height >= 768) {
-            move_uploaded_file($teamImage['tmp_name'], $uploadPath);
-
-            $scrumMasterID = $_SESSION['id'];
-            $projectIDQuery = "SELECT id FROM projects WHERE scrumMasterID = ?";
-            $stmtProjectID = $conn->prepare($projectIDQuery);
-
-            $stmtProjectID->bind_param("i", $scrumMasterID);
-            $stmtProjectID->execute(); 
-
-            $stmtProjectID->bind_result($projectID);
-            $stmtProjectID->fetch();
-            $stmtProjectID->close();
-
-            $insertSql = "INSERT INTO teams (image, description, teamName, projectID, scrumMasterID) VALUES (?, ?, ?, ?, ?)";
-            $stmt = $conn->prepare($insertSql);
-            $stmt->bind_param("sssss", $uploadPath, $formDescription, $formName, $projectID, $scrumMasterID);
-            $stmt->execute();
-            $stmt->close();
-
-            header('Location: dashboardScrum.php');
-            exit;
-        } else {
-            echo 'Invalid image dimensions. Please upload an image with a minimum size of 1152x768 pixels.';
-        }
-    } else {
-        echo 'Invalid file. Please upload a valid image file (JPEG or PNG) with a size less than 1MB.';
-    }
+    $scrumMaster = new ScrumMaster($conn);
+    $scrumMaster->createTeam($_POST['formName'], $_POST['formDescription'], $_FILES['teamImage']);
 }
 ?>
--- includes/scrumMaster.php
+++ includes/scrumMaster.php
@@ -50,6 +50,46 @@ class ScrumMaster extends User {
         }
     }
 
+    public function createTeam($formName, $formDescription, $teamImage) {
+        session_start();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $uploadDir = './img/';
+            $uploadPath = $uploadDir . basename($teamImage['name']);
+
+            if ($teamImage['size'] > 0 && $teamImage['size'] <= 10 * 1024 * 1024 && ($teamImage['type'] === 'image/jpeg' || $teamImage['type'] === 'image/png')) {
+                list($width, $height) = getimagesize($teamImage['tmp_name']);
+                if ($width >= 1152 && $height >= 768) {
+                    move_uploaded_file($teamImage['tmp_name'], $uploadPath);
+
+                    $scrumMasterID = $_SESSION['id'];
+                    $projectIDQuery = "SELECT id FROM projects WHERE scrumMasterID = ?";
+                    $stmtProjectID = $this->conn->prepare($projectIDQuery);
+
+                    $stmtProjectID->bind_param("i", $scrumMasterID);
+                    $stmtProjectID->execute(); 
+
+                    $stmtProjectID->bind_result($projectID);
+                    $stmtProjectID->fetch();
+                    $stmtProjectID->close();
+
+                    $insertSql = "INSERT INTO teams (image, description, teamName, projectID, scrumMasterID) VALUES (?, ?, ?, ?, ?)";
+                    $stmt = $this->conn->prepare($insertSql);
+                    $stmt->bind_param("sssss", $uploadPath, $formDescription, $formName, $projectID, $scrumMasterID);
+                    $stmt->execute();
+                    $stmt->close();
+
+                    header('Location: dashboardScrum.php');
+                    exit;
+                } else {
+                    echo 'Invalid image dimensions. Please upload an image with a minimum size of 1152x768 pixels.';
+                }
+            } else {
+                echo 'Invalid file. Please upload a valid image file (JPEG or PNG) with a size less than 1MB.';
+            }
+        }
+    }
+
     public function deleteTeam($teamId) {
         $currentMemberID = $_SESSION['id'];
 
