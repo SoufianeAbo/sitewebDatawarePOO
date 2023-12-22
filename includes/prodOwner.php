8453cb4cb4b0de5d6215e6a469a8ddb619790b80
SoufianeAbo <abounasrsoufiane@gmail.com>
2023-12-22 16:43:02 +0100

    finish making modify project function for product owner

--- includes/prodOwner.php
+++ includes/prodOwner.php
@@ -135,4 +135,42 @@ class ProdOwner extends User {
             echo 'Invalid file. Please upload a valid image file (JPEG or PNG) with a size less than 1MB.';
         }
     }
+
+    public function updateProjectDetails($formName, $formDescription, $formDate, $selectedModify, $teamImage) {
+        $uploadDir = './img/';
+        $uploadPath = $uploadDir . basename($teamImage['name']);
+
+        if ($teamImage['size'] > 0 && $teamImage['size'] <= 10 * 1024 * 1024 && ($teamImage['type'] === 'image/jpeg' || $teamImage['type'] === 'image/png')) {
+            list($width, $height) = getimagesize($teamImage['tmp_name']);
+
+            if ($width >= 1152 && $height >= 768) {
+                move_uploaded_file($teamImage['tmp_name'], $uploadPath);
+
+                $scrumMasterID = $_SESSION['id'];
+                $projectIDQuery = "SELECT id FROM projects WHERE scrumMasterID = ?";
+                $stmtProjectID = $this->conn->prepare($projectIDQuery);
+
+                $stmtProjectID->bind_param("i", $scrumMasterID);
+                $stmtProjectID->execute();
+
+                $stmtProjectID->bind_result($projectID);
+                $stmtProjectID->fetch();
+                $stmtProjectID->close();
+                $currentMemberID = $_SESSION['id'];
+
+                $updateSql = "UPDATE projects SET image=?, name=?, description=?, date_end=? WHERE id=?";
+                $stmt = $this->conn->prepare($updateSql);
+                $stmt->bind_param("ssssi", $uploadPath, $formName, $formDescription, $formDate, $selectedModify);
+                $stmt->execute();
+                $stmt->close();
+
+                header('Location: dashboardProd.php');
+                exit;
+            } else {
+                echo 'Invalid image dimensions. Please upload an image with a minimum size of 1152x768 pixels.';
+            }
+        } else {
+            echo 'Invalid file. Please upload a valid image file (JPEG or PNG) with a size less than 1MB.';
+        }
+    }
 }
